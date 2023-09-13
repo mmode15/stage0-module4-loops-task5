d711872fed264ce28d8b36b234a9a613d2a56eef
Maiko <maiko.mode05@gmail.com>
2023-09-13 14:38:21 +0400

    done

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task5;
 
 public class Cross {
+
     public void printCross(int sideLength) {
+        for(int  i= 0; i< sideLength ; i++){
+            for(int  j= 0; j< sideLength ; j++){
+              if(i == Math.abs(sideLength/2)){
+                  System.out.print("8");
+              }else if(j == Math.abs(sideLength/2)){
+                  System.out.print("8");
+              }else{
+                  System.out.print(" ");
+              }
+            }
+            System.out.println();
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
 
+    public   void printTriangle(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }
